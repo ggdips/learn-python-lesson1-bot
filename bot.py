b10d9875a503431416143dfd249e17dbb71dc3b4
Дмитрий Корняков <kornyakovdy@nspk.ru>
2021-11-29 12:43:16 +0300

    Added venv

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3.8
-
+#!env/bin/python3
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
+import ephem
+from datetime import date
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
@@ -15,10 +16,19 @@ def talk_to_me(update, context):
     logging.info(user_text)
     update.message.reply_text(user_text)
 
+def where_is_planet(update, context):
+    logging.info('Вызван /planet')
+    planet = update.message.text.split()[1]
+    constellation = ephem.constellation
+    p = getattr(ephem, planet)
+    constellation = ephem.constellation(p(date.today()))
+    update.message.reply_text(f'{planet} in {constellation[1]}')
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", where_is_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info("Бот стартовал")
     mybot.start_polling()
